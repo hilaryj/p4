c2e501bd8cb4bf24407551505dbc9a9a70a820b7
hilaryj <hilaryj@users.noreply.github.com>
2014-10-28 13:26:05 -0400

    practicing stuff from class...

--- app/routes.php
+++ app/routes.php
@@ -32,3 +32,15 @@ Route::post('/generate', function() {
 
 
 # Login page?
+Route::get('mysql-test', function() {
+
+    # Print environment
+    echo 'Environment: '.App::environment().'<br>';
+
+    # Use the DB component to select all the databases
+    $results = DB::select('SHOW DATABASES;');
+
+    # If the "Pre" package is not installed, you should output using print_r instead
+    echo Pre::render($results);
+
+});
