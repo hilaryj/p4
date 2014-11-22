91b69452ebf9e8b42a513746fd69a6466c3a04c4
Hilary <hilary.m.johnson@gmail.com>
2014-11-22 18:41:32 -0500

    updaing routes

--- app/routes.php
+++ app/routes.php
@@ -9,36 +9,6 @@ Route::post('/', function() {
 
 });
 
-Route::get('/practice', function() {
-
-    $fruit = Array('Apples', 'Oranges', 'Pears');
-
-    echo Pre::render($fruit,'Fruit');
-
-});
-
-
-
-Route::get('/get-environment',function() {
-
-    echo "Environment: ".App::environment();
-
-});
-
-Route::get('mysql-test', function() {
-
-    # Print environment
-    echo 'Environment: '.App::environment().'<br>';
-
-    # Use the DB component to select all the databases
-    $results = DB::select('SHOW DATABASES;');
-
-    # If the "Pre" package is not installed, you should output using print_r instead
-    echo Pre::render($results);
-
-});
-
-# /app/routes.php
 Route::get('/debug', function() {
 
     echo '<pre>';
@@ -81,5 +51,34 @@ Route::get('/debug', function() {
     }
 
     echo '</pre>';
+
+});
+
+
+Route::get('/practice', function() {
+
+    $fruit = Array('Apples', 'Oranges', 'Pears');
+
+    echo Pre::render($fruit,'Fruit');
+
+});
+
+
+Route::get('/get-environment',function() {
+
+    echo "Environment: ".App::environment();
+
+});
+
+Route::get('mysql-test', function() {
+
+    # Print environment
+    echo 'Environment: '.App::environment().'<br>';
+
+    # Use the DB component to select all the databases
+    $results = DB::select('SHOW DATABASES;');
+
+    # If the "Pre" package is not installed, you should output using print_r instead
+    echo Pre::render($results);
 
 });
